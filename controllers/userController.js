80be0e9b9892b491b5d2ed4ad7acc489e94867b5
firas hamila <firas.hm.1996@gmail.com>
2025-02-25 14:14:16 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,3 +31,20 @@ exports.updateUser = async (req, res) => {
     });
   }
 };
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "user not found !!" });
+    }
+    res.status(204).json({
+      message: "User updated !!!",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!",
+      error: error,
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { createUser, updateUser } = require("../controllers/userController");
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 const Routes = express.Router();
 
 Routes.route("/").post(createUser);
 Routes.route("/:id").patch(updateUser);
+Routes.route("/:id").delete(deleteUser);
 
 module.exports = Routes;
